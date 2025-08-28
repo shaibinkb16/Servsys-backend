9a228e66b35ecf6152353ef77a832bf47746a927
shaibin <shaibin16@gmail.com>
2025-08-29 00:18:24 +0530

    fifth

--- app/main.py
+++ app/main.py
@@ -288,5 +288,6 @@ async def check_renewal_notifications_route(background_tasks: BackgroundTasks):
 
 
 if __name__ == "__main__":
-	import uvicorn
-	uvicorn.run(app, host="0.0.0.0", port=8000)
+    import uvicorn
+    port = int(os.getenv("PORT", 8000))  # Render provides PORT, default to 8000 locally
+    uvicorn.run(app, host="0.0.0.0", port=port)
